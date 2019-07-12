b68bb586737c7aeef19241a15186fabb56e5e27d
annadamczyk <annam42m17@gmail.com>
2019-07-12 08:07:16 +0200

    change of search

--- WebApplication29/App/Controller.js
+++ WebApplication29/App/Controller.js
@@ -1,7 +1,7 @@
 ﻿app.controller('AppController', function ($scope, AppService)
 {
 	var maxTimestamp;
-	$scope.filtersName = ["logger","level","timestamp", "thread"]
+	$scope.filtersName = ["logger","level","timestamp", "thread", "message"]
 	$scope.selectedNames = ["logger","level","timestamp"]
 	$scope.threadRanges = ["empty","<=10","10-40","40-80","80-110", ">110"]
 
@@ -38,6 +38,35 @@
 	function readFiltredLogs(){
 		$scope.logsData = AppService.getFilteredLogs();   
 		$scope.$applyAsync();
+	}
+
+	$scope.filterAngular = function(){
+		return function(item){
+		if($scope.filterColumn === "level"){
+			return item.level.includes($scope.filterText.toUpperCase()) || item.level.includes($scope.filterText.toLowerCase()); 
+		}else if($scope.filterColumn === "thread"){
+			return item.thread === $scope.filterText;
+		}else if($scope.filterColumn === "logger"){
+			return item.logger.includes($scope.filterText.toUpperCase()) || item.logger.includes($scope.filterText.toLowerCase());
+		}else if($scope.filterColumn === "message"){
+			return item.message.includes($scope.filterText.toUpperCase()) || item.message.includes($scope.filterText.toLowerCase());
+		}else if($scope.threadColumn === 'empty' && ($scope.filterColumn === "" || typeof $scope.filterColumn === 'undefined')){
+			return item;
+		}
+		if($scope.threadColumn !== 'empty'){
+			if($scope.threadColumn === "<=10"){
+				return item.thread <= 10;
+			}else if($scope.threadColumn === "10-40"){
+				return item.thread >10 && item.thread <= 40;
+			}else if($scope.threadColumn === "40-80"){
+				return item.thread > 40 && item.thred <= 80;
+			}else if($scope.threadColumn === "80-110"){
+				return item.thread >80 && item.thread <=110;
+			}else if($scope.threadColumn === ">110"){
+				return item.thread > 110;
+			}
+		}
+		}
 	}
 
 	function refreshLogs(){
